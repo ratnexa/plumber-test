d3d3e4efd18969531a9525a2e4949e77f1e3c1f3
Merqueo <samezquita@merqueo.com>
2020-09-03 12:08:12 -0500

    use port 40

--- runAPI.R
+++ runAPI.R
@@ -1,6 +1,6 @@
 library(plumber)
 r <- plumb(file = 'plumber.R')
-r$run(host = "0.0.0.0", port = 5000)
+r$run(host = "0.0.0.0", port = 40)
 
 #POST(url = 'http://127.0.0.1:8000/predict?am=1&qsec=16.46&wt=2.62') %>% content()
 
